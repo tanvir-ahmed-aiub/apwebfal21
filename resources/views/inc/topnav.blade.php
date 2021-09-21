29a360cd0ec189fc92dd1ca00b17d0bbb14db68c
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2021-09-21 13:56:02 +0600

    layouts blade template

---
+++ resources/views/inc/topnav.blade.php
@@ -0,0 +1,3 @@
+<a class="btn btn-primary" href="{{route('contact')}}">Contact</a>
+<a class="btn btn-success" href="{{route('profile')}}">My Profile</a>
+<a class="btn btn-danger" href="{{route('home')}}"> Home </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,7 @@
 
     </head>
     <body>
-        <a class="btn btn-primary" href="{{route('contact')}}">Contact</a>
-        <a class="btn btn-success" href="{{route('profile')}}">My Profile</a>
-        <a class="btn btn-danger" href="{{route('home')}}"> Home </a>
+        @include('inc.topnav')
         <div>
             @yield('content')
         </div>
